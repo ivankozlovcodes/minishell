04032d033585c6e0c96898cb63ab25b5d2ff0086
Ivan Kozlov <ivaan.kozlov@gmail.com>
2019-05-21 06:24:16 -0700

    Added prompt and parsing input from user

--- includes/minishell.h
+++ includes/minishell.h
@@ -6,7 +6,7 @@
 /*   By: ivankozlov <ivankozlov@student.42.fr>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/05/21 02:58:51 by ivankozlov        #+#    #+#             */
-/*   Updated: 2019/05/21 03:36:16 by ivankozlov       ###   ########.fr       */
+/*   Updated: 2019/05/21 06:21:57 by ivankozlov       ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,6 +14,8 @@
 # define MINISHELL_H
 
 # define USAGE "usage"
+
+# define CWD_BUFF_SIZE 2048
 
 struct					s_env
 {
@@ -25,5 +27,7 @@ typedef struct s_env	t_env;
 extern t_env			*g_env;
 
 void					init_env(void);
+
+void					display_prompt(void);
 
 #endif
--- src/prompt.c
+++ src/prompt.c
@@ -1,46 +1,25 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   main.c                                             :+:      :+:    :+:   */
+/*   prompt.c                                           :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: ivankozlov <ivankozlov@student.42.fr>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2019/05/21 02:31:25 by ivankozlov        #+#    #+#             */
-/*   Updated: 2019/05/21 06:23:19 by ivankozlov       ###   ########.fr       */
+/*   Created: 2019/05/21 06:00:59 by ivankozlov        #+#    #+#             */
+/*   Updated: 2019/05/21 06:18:09 by ivankozlov       ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <unistd.h>
 
 #include "minishell.h"
+#include "colors.h"
 #include "ft_printf.h"
-#include "dstring.h"
-#include "memory.h"
 
-static char		*get_input(void)
+void	display_prompt(void)
 {
-	t_string	*s;
-	char		buf[2];
-	int			ret;
+	char	cwd[CWD_BUFF_SIZE + 1];
 
-	buf[1] = 0;
-	s = string_init(0);
-	while ((ret = read(0, buf, 1)) && buf[0] != '\n')
-		string_append(s, buf);
-	return (string_destroy(s, TRUE));
-}
-
-int				main(void)
-{
-	char	*input;
-
-	init_env();
-	while (1)
-	{
-		display_prompt();
-		input = get_input();
-		ft_printf("input |%s|\n", input);
-		ft_free(1, input);
-	}
-	return (0);
+	getcwd(cwd, CWD_BUFF_SIZE);
+	ft_printf("%s%s%s > ", BLU, cwd, RESET);
 }
