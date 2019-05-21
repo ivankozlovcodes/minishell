4ddc3a059091441799a8695d7f87619e88ca7fa5
Ivan Kozlov <ivaan.kozlov@gmail.com>
2019-05-21 04:47:34 -0700

    Added initial env parsing

--- includes/minishell.h
+++ includes/minishell.h
@@ -6,7 +6,7 @@
 /*   By: ivankozlov <ivankozlov@student.42.fr>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/05/21 02:58:51 by ivankozlov        #+#    #+#             */
-/*   Updated: 2019/05/21 03:00:45 by ivankozlov       ###   ########.fr       */
+/*   Updated: 2019/05/21 03:36:16 by ivankozlov       ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,5 +14,16 @@
 # define MINISHELL_H
 
 # define USAGE "usage"
+
+struct					s_env
+{
+	char	*name;
+	char	*value;
+};
+typedef struct s_env	t_env;
+
+extern t_env			*g_env;
+
+void					init_env(void);
 
 #endif
