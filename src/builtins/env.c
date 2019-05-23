6823ea23f3b19fdfd8e85ee6e712b93df469f45d
Ivan Kozlov <ivaan.kozlov@gmail.com>
2019-05-23 13:25:59 -0700

    Implemented env builtin

--- src/builtins/env.c
+++ src/builtins/env.c
@@ -6,14 +6,19 @@
 /*   By: ivankozlov <ivankozlov@student.42.fr>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/05/21 07:22:22 by ivankozlov        #+#    #+#             */
-/*   Updated: 2019/05/21 07:22:26 by ivankozlov       ###   ########.fr       */
+/*   Updated: 2019/05/23 13:25:32 by ivankozlov       ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include "ft_printf.h"
 #include "minishell.h"
 
 int		env(char *name, char **args)
 {
-	not_implemented(name);
+	size_t		i;
+
+	i = -1;
+	while (g_env[++i].name)
+		ft_printf("%s=%s\n", g_env[i].name, g_env[i].value);
 	return (0);
 }
