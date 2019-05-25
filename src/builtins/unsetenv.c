0a6de128b4f0ab0df4818ff6e68bdadb368dcc57
Ivan Kozlov <ivaan.kozlov@gmail.com>
2019-05-25 04:23:13 -0700

    Implemented unsetenv builtin

--- src/builtins/unsetenv.c
+++ src/builtins/unsetenv.c
@@ -6,14 +6,22 @@
 /*   By: ivankozlov <ivankozlov@student.42.fr>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/05/21 07:21:29 by ivankozlov        #+#    #+#             */
-/*   Updated: 2019/05/21 07:21:40 by ivankozlov       ###   ########.fr       */
+/*   Updated: 2019/05/25 04:27:11 by ivankozlov       ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include "ft_printf.h"
 #include "minishell.h"
 
 int		unsetenv(char *name, char **args)
 {
-	not_implemented(name);
+	if (!*args)
+		ft_printf("unsetenv: Too few arguments.\n");
+	else
+		while (*args)
+		{
+			dict_remove(g_env, *args);
+			args++;
+		}
 	return (0);
 }
