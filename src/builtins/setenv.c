269213b1e1f2eadc9491ee837a0c139f8222ccfc
Ivan Kozlov <ivaan.kozlov@gmail.com>
2019-05-25 04:13:27 -0700

    Implemented setenv builtin

--- src/builtins/setenv.c
+++ src/builtins/setenv.c
@@ -6,14 +6,25 @@
 /*   By: ivankozlov <ivankozlov@student.42.fr>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/05/21 07:22:03 by ivankozlov        #+#    #+#             */
-/*   Updated: 2019/05/21 07:22:12 by ivankozlov       ###   ########.fr       */
+/*   Updated: 2019/05/25 04:28:38 by ivankozlov       ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include "ftstring.h"
+#include "ft_printf.h"
 #include "minishell.h"
 
 int		setenv(char *name, char **args)
 {
-	not_implemented(name);
+	char	*env_name;
+	char	*env_value;
+
+	(void)name;
+	env_name = *args;
+	env_value = *(args + 1) ? *(args + 1) : "";
+	if (!env_name)
+		return (env(name, args));
+	else
+		dict_insert(g_env, env_name, env_value);
 	return (0);
 }
