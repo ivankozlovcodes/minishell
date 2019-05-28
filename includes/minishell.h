a7e4d4534ef41a33a205f2f5fbe97a7204a33bc2
Ivan Kozlov <ivaan.kozlov@gmail.com>
2019-05-28 14:04:49 -0700

    Moved get_env_name.

--- includes/minishell.h
+++ includes/minishell.h
@@ -6,7 +6,7 @@
 /*   By: ivankozlov <ivankozlov@student.42.fr>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/05/21 02:58:51 by ivankozlov        #+#    #+#             */
-/*   Updated: 2019/05/27 03:34:32 by ivankozlov       ###   ########.fr       */
+/*   Updated: 2019/05/28 13:52:54 by ivankozlov       ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -70,6 +70,7 @@ int						run_cmd(char *name, char **args);
 int						unsetenv(char *name, char **args);
 
 char					*remove_quotes(char *s);
+char					*get_env_name(char *arg);
 size_t					token_len(const void *token);
 
 void					not_implemented(char *name);
