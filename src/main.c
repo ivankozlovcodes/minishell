c1d0eb30fe4edcd84efe490168d655014492c984
Ivan Kozlov <ivaan.kozlov@gmail.com>
2019-05-27 17:23:51 -0700

    Fixed issue: executing old command when press enter

--- src/main.c
+++ src/main.c
@@ -6,7 +6,7 @@
 /*   By: ivankozlov <ivankozlov@student.42.fr>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/05/21 02:31:25 by ivankozlov        #+#    #+#             */
-/*   Updated: 2019/05/27 03:35:16 by ivankozlov       ###   ########.fr       */
+/*   Updated: 2019/05/27 17:22:55 by ivankozlov       ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -56,6 +56,7 @@ int				main(void)
 		ret = exec_input(input);
 		print_minishell_error();
 		ft_free(1, input);
+		input = NULL;
 		if (ret == EXIT_SIGNAL)
 			break ;
 	}
